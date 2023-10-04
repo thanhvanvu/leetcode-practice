99092e5be117a104e87d3a688b29507ab8b89ced
Thanh Vu <vvt4994@gmail.com>
2023-10-04 12:47:59 -0500

    896: Monotonic Array

---
+++ leetcode_problem/896_monotonic_array/896_monotonic_array.py
@@ -0,0 +1,63 @@
+# https://www.youtube.com/watch?v=sqWOFIZ9Z0U&t=388s
+# https://leetcode.com/problems/monotonic-array/description/
+
+def isMonotonicMySolotion(nums):
+    i = 1
+
+    # [1] ==> true
+    if len(nums) == 1:
+        return True
+
+    while i < len(nums):
+        # [1,1,1,2,3]
+        # if left == right ==> i + 1
+        if nums[i] == nums[i - 1]:
+            i += 1
+
+            # case [1,1,1]
+            if i == len(nums):
+                return True
+        else:
+            # increase
+            if nums[i] > nums[i - 1]:
+                r = i
+                while r < len(nums) - 1:
+                    # if left > right ==> decrease ==> False
+                    if nums[r] > nums[r + 1]:
+                        return False
+                    r += 1
+                return True
+
+            # decrease
+            if nums[i] < nums[i - 1]:
+                r = i
+                while r < len(nums) - 1:
+                    # if left < right ==> increase ==> false
+                    if nums[r] < nums[r + 1]:
+                        return False
+                    r += 1
+                return True
+
+
+def isMonotonic(nums):
+    increase = True  # Assume it's increasing by default
+    decrease = True  # Assume it's decreasing by default
+
+    for i in range(len(nums) - 1):
+        # [1,2,3,0,4] ==> increase = False at 0
+        if not nums[i] <= nums[i + 1]:
+            increase = False
+
+        # [6,5,3,2,9,4] ==> decrease = False at 9
+        if not nums[i] >= nums[i + 1]:
+            decrease = False
+
+    # true or true ==> true
+    # true or false ==> false
+    # false or false ==> false
+    return increase or decrease
+
+
+nums = [1,3,2]
+
+print(isMonotonic(nums))
