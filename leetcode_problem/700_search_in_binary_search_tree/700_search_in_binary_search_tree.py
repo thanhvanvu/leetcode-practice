35cd120661e652b72788963a4e80a4a01ab4e315
Thanh Vu <vvt4994@gmail.com>
2023-10-12 05:03:10 -0500

    700. Search in a Binary Search Tree

---
+++ leetcode_problem/700_search_in_binary_search_tree/700_search_in_binary_search_tree.py
@@ -0,0 +1,39 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def print_subtree(root):
+    if root is not None:
+        print_subtree(root.left)
+        print(root.val, end=' ')
+        print_subtree(root.right)
+
+
+# Helper function to build a sample binary search tree
+def build_sample_tree():
+    # Create nodes for a sample BST
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    return root
+
+
+def searchBST(root, val: int):
+    while root is not None:
+        if val < root.val:
+            root = root.left
+        elif val > root.val:
+            root = root.right
+        else:
+            return root
+
+
+# Create a sample binary search tree
+root = build_sample_tree()
+
+print(print_subtree(searchBST(root, 2)))
