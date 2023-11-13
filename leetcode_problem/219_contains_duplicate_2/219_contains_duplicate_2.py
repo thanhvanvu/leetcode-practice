317a45e8781acb06cca78e09d4ccf7cab7ca3129
Thanh Vu <vvt4994@gmail.com>
2023-11-12 19:10:33 -0600

    219. Contains Duplicate II

---
+++ leetcode_problem/219_contains_duplicate_2/219_contains_duplicate_2.py
@@ -0,0 +1,22 @@
+def containsNearbyDuplicate(nums, k):
+    window = set()
+    L = 0
+
+    for R in range(len(nums)):
+        # case window is too big
+        if R - L > k:
+            L += 1
+            window.remove(nums[L])
+
+        if nums[R] in window:
+            return True
+
+        # if nums[R] is not in window
+        # add to set
+        window.add(nums[R])
+
+    return False
+
+nums = [1, 2, 3, 1]
+k = 3
+print(containsNearbyDuplicate(nums, k))
