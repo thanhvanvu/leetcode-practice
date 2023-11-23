dddd66bbc25919d0eaa917500fbe0ee1c8a616c1
Thanh Vu <vvt4994@gmail.com>
2023-11-23 03:54:02 -0600

    102. Binary Tree Level Order Traversal

---
+++ leetcode_problem/102_binary_tree_level_order_traversal/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+# https://www.youtube.com/watch?v=6ZnyEApgFYg
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# iterative solution
+
+
+def levelOrder(root):
+    queue = deque()
+    result = []
+    if root is not None:
+        queue.append(root)
+
+    level = 0
+
+    while len(queue) > 0:
+        print("level: ", level)
+
+        level_array = []
+        for i in range(len(queue)):
+
+            curr = queue.popleft()
+            print(curr.val)
+
+            level_array.append(curr.val)
+
+            # should append left -> right to queue
+            if curr.left:
+                queue.append(curr.left)
+
+            if curr.right:
+                queue.append(curr.right)
+
+        level += 1
+        result.append(level_array)
+
+    return result
+
+
+# Helper function to build a sample binary search tree
+def build_sample_tree():
+    # Create nodes for a sample BST
+    root = TreeNode(4)
+    root.left = TreeNode(3)
+    root.right = TreeNode(6)
+    root.left.left = TreeNode(2)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(7)
+    return root
+
+
+# Create a sample binary search tree
+root = build_sample_tree()
+
+print(levelOrder(root))
