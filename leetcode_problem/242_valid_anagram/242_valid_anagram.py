c3cd3d84760b20f197379f5b2d0da17de8cf7e7c
Thanh Vu <vvt4994@gmail.com>
2024-10-25 15:11:42 -0500

    242. Valid Anagram practice

--- leetcode_problem/242_valid_anagram/242_valid_anagram.py
+++ leetcode_problem/242_valid_anagram/242_valid_anagram.py
@@ -31,7 +31,40 @@ def isAnagram(s, t):
     return True
 
 
-s_string = "rat"
-t_string = "car"
+def isAnagram2(s, t):
+    # count every character and store in hashmap
+    countS = {}
+    countT = {}
 
-print(isAnagram(s_string, t_string))
+    # if 2 strings have different length -> return False
+    if len(s) != len(t):
+        return False
+
+    # count all char in s
+    for char in range(len(s)):
+        if s[char] not in countS:
+            countS[s[char]] = 1
+        else:
+            countS[s[char]] += 1
+
+    # count all char in t
+    for char in range(len(t)):
+        if t[char] not in countT:
+            countT[t[char]] = 1
+        else:
+            countT[t[char]] += 1
+
+    # compare 2 hashmap
+    # countS = {'r': 1, 'a': 1}
+    # countT = {'c': 1, 'a': 1}
+    # r is not in count T -> return false
+    for c in countS:
+        if (c not in countT) or (c not in countS) or (countS[c] != countT[c]):
+            return False
+
+    return True
+
+
+s_string = "rat"
+t_string = "tar"
+print(isAnagram2(s_string, t_string))
