e6d457411e5094cd2535acb22513bc29d5b88b43
Thanh Vu <vvt4994@gmail.com>
2024-10-29 14:26:19 -0500

    1299. Replace element with greatest element on right side

---
+++ leetcode_problem/1299_Replace_elements_with_greatest_element_on_right_side/1299.py
@@ -0,0 +1,30 @@
+def replaceElements_bruteForce(arr):
+    for n in range(len(arr)):
+        greatest = 0
+        k = n + 1
+        if k == len(arr):
+            arr[n] = -1
+            return arr
+        while k < len(arr):
+            greatest = max(arr[k], greatest)
+            k += 1
+
+        arr[n] = greatest
+
+
+def replaceElements(arr):
+    # initial max base case = -1
+    # reverse loop
+    # new max = max(oldMax, arr[i])
+
+    rightMax = -1
+    for i in range(len(arr) - 1, -1, -1):
+        newMax = max(arr[i], rightMax)
+        arr[i] = rightMax
+        rightMax = newMax
+
+    return arr
+
+
+array = [17, 18, 5, 4, 6, 1]
+print(replaceElements(array))
