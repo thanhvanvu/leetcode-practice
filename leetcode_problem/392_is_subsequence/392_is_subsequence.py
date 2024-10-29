d98b38f89d4bfbb95cf63eaaa0218cd2a24df535
Thanh Vu <vvt4994@gmail.com>
2024-10-29 14:50:49 -0500

    Update 392_is_subsequence.py

--- leetcode_problem/392_is_subsequence/392_is_subsequence.py
+++ leetcode_problem/392_is_subsequence/392_is_subsequence.py
@@ -1,4 +1,4 @@
-def isSubsequence(s: str, t: str) -> bool:
+def isSubsequence1(s: str, t: str) -> bool:
     i = 0  # pointer for t
     j = 0  # pointer for s
 
@@ -13,7 +13,24 @@ def isSubsequence(s: str, t: str) -> bool:
         return False
 
 
-s = ""
+def isSubsequence(s: str, t: str):
+    # use 2 pointer to check every character from 2 sting
+    i, j = 0, 0
+
+    # base case
+    if len(s) == 0 or len(t) == 0:
+        return False
+
+    for j in range(len(t)):
+        if s[i] == t[j]:
+            i += 1
+        if i == len(s):
+            return True
+
+    return False
+
+
+s = "axc"
 t = "ahbgdc"
 
 print(isSubsequence(s, t))
