e8cf2d1efaa447c911dec1b00e12c845e5f03d0f
Thanh Vu <vvt4994@gmail.com>
2024-10-30 14:28:38 -0500

    205. Isomorphic Strings

---
+++ leetcode_problem/205_isomorphic_strings/205_isomorphic_strings.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/unique-email-addresses/description/
+# https://www.youtube.com/watch?v=TC_xLIWl7qY
+
+def isIsomorphic(s, t):
+    # 2 string will always have the same length
+    # Map char in S to char in T
+    # Map char in T to char in S
+
+    mapST, mapTS = {}, {}
+
+    for i in range(len(s)):
+        char1, char2 = s[i], t[i]
+
+        # check if character is in hashmap or not first
+        # if char1, char2 are not in hashmap yet -> insert it to hashmap
+        if (char1 in mapST and mapST[char1] != char2) or (char2 in mapTS and mapTS[char2] != char1):
+            return False
+
+        mapST[char1] = char2
+        mapTS[char2] = char1
+
+    return True
+
+
+a = "foo"
+b = "bar"
+print(isIsomorphic(a, b))
