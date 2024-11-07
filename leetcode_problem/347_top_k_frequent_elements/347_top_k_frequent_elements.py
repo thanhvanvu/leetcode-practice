b8d26bcd827bda698c862f1e507b231aa1a7acf1
Thanh Vu <vvt4994@gmail.com>
2024-11-06 18:50:08 -0600

    Update 347_top_k_frequent_elements.py

--- leetcode_problem/347_top_k_frequent_elements/347_top_k_frequent_elements.py
+++ leetcode_problem/347_top_k_frequent_elements/347_top_k_frequent_elements.py
@@ -32,7 +32,41 @@ def topKFrequent(nums, k):
                 return result
 
 
-list_nums = [1, 1, 1, 2, 2, 3]
+def topKFrequent_practice(nums, k):
+    # [[], [], [], [], [], []]
+    freq = [[] for i in range(len(nums) + 1)]
+
+    # count number
+    count = {}
+
+    output = []
+
+    for i in nums:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
+
+    # bucket sort
+    for element in count:
+        key = element
+        val = count[element]
+
+        freq[val].append(key)
+
+    print(freq)
+    # [[], [3], [2], [1], [], []]
+    # it means: 1 occurs 3 times, 2 occurs 2 times, 3 occurs 1 time
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            output.append(n)
+
+            # check if result has k most frequent elements.
+            if len(output) == k:
+                return output
+
+
+list_nums = [1,1,1,2,2,2,3]
 value = 2
 
-print(topKFrequent(list_nums, value))
+print(topKFrequent_practice(list_nums, value))
