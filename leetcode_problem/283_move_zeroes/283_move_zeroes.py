318e52f50bf1457f4b1a609e53b7d723c48ce8e0
Thanh Vu <vvt4994@gmail.com>
2024-11-12 16:37:58 -0600

    Update 283_move_zeroes.py

--- leetcode_problem/283_move_zeroes/283_move_zeroes.py
+++ leetcode_problem/283_move_zeroes/283_move_zeroes.py
@@ -10,5 +10,19 @@ def moveZeroes(nums):
     return nums
 
 
-nums = [0]
-print(moveZeroes(nums))
+def moveZeroes_practice(nums):
+    l = 0
+    for r in range(len(nums)):
+        if nums[r] != 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+
+    return nums
+
+
+nums = [0, 1, 0, 3, 12]
+
+# move all 0's to the end of it while maintaining the relative order
+# should understand it different way
+# move all number which is != 0 to the front
+print(moveZeroes_practice(nums))
