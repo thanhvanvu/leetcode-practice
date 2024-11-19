1e3f79207f1654c7826a6e17f9499fa5a078f781
Thanh Vu <vvt4994@gmail.com>
2024-11-19 16:15:06 -0600

    Update 122_best_time_to_buy_and_sell_stock_2.py

--- leetcode_problem/122_best_time_to_buy_and_sell_stock_2/122_best_time_to_buy_and_sell_stock_2.py
+++ leetcode_problem/122_best_time_to_buy_and_sell_stock_2/122_best_time_to_buy_and_sell_stock_2.py
@@ -11,5 +11,19 @@ def maxProfit(prices):
     return maxProf
 
 
-prices = [7, 6, 4, 3, 1]
-print(maxProfit(prices))
+def maxProfit_practice(prices):
+    # do not need 2 pointers
+    # just loop through prices
+    # check if i > i-1
+    # calculate prices[i] - prices[i-1] and add to maxProf
+    maxPro = 0
+    for i in range(1, len(prices)):
+        if prices[i] >= prices[i - 1]:
+            profit = prices[i] - prices[i - 1]
+            maxPro += profit
+
+    return maxPro
+
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit_practice(prices))
