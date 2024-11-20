db1e21c89556ab8de47a26202fa5b8329d099d99
Thanh Vu <vvt4994@gmail.com>
2024-11-20 17:09:30 -0600

    Update 219_contains_duplicate_2.py

--- leetcode_problem/219_contains_duplicate_2/219_contains_duplicate_2.py
+++ leetcode_problem/219_contains_duplicate_2/219_contains_duplicate_2.py
@@ -17,6 +17,33 @@ def containsNearbyDuplicate(nums, k):
 
     return False
 
-nums = [1, 2, 3, 1]
-k = 3
-print(containsNearbyDuplicate(nums, k))
+
+def containsNearbyDuplicate_practice(nums, k):
+    L = 0
+
+    # numSet to check duplicate
+    window = set()
+
+    for R in range(len(nums)):
+        # window must be <= k
+        # window = R - L = 4 - 0 = 4 > k
+        # remove the most left element from nums in window
+        # change window
+        if R - L > k:
+            window.remove(nums[L])
+            L += 1
+
+        # found duplicate
+        if nums[R] in window:
+            return True
+
+        # if element is not in set() -> add it to set
+        window.add(nums[R])
+
+    # for loop is done -> no duplicate
+    return False
+
+
+nums = [1, 2, 3, 1, 2, 3]
+k = 2
+print(containsNearbyDuplicate_practice(nums, k))
