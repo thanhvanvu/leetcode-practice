12c38732008c7936cfd9d8e897d005f8e3c6d74d
Thanh Vu <vvt4994@gmail.com>
2024-11-20 23:36:59 -0600

    1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ leetcode_problem/1456_maximum_number_of_vowels_in_a_substring_of_given_length/1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -0,0 +1,42 @@
+def maxVowels(s: str, k: int) -> int:
+    vowel = {"a", "e", "i", "o", "u"}
+    maximum = 0
+    count = 0
+
+    L = 0
+
+    for R in range(len(s)):
+
+        # calculate window
+        window = R - L + 1
+
+        # if window > k
+        # shrink window until condition meets
+        # if the most left element is not in vowel
+        # decrease count
+        if window > k:
+            if s[L] in vowel:
+                count -= 1
+            L += 1
+
+        char = s[R]
+
+        # if element in vowel
+        # count +1
+        if char in vowel:
+            count += 1
+
+        # calculate maximum
+        maximum = max(maximum, count)
+
+        # if we see count == window
+        # just return it immediately
+        if count == k:
+            return count
+    
+    return maximum
+
+
+s = "zpuerktejfp"
+k = 3
+print(maxVowels(s, k))
