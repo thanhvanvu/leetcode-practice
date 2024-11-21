b0f7e26e2c0dca39e1a09142dfb62e8f9e0b7635
Thanh Vu <vvt4994@gmail.com>
2024-11-20 19:12:19 -0600

    Update 424_longest_repeating_character_replacement.py

--- leetcode_problem/424_longest_repeating_character_replacement/424_longest_repeating_character_replacement.py
+++ leetcode_problem/424_longest_repeating_character_replacement/424_longest_repeating_character_replacement.py
@@ -29,8 +29,43 @@ def characterReplacement(s, k):
 
     return result
 
+def characterReplacement_practice(s, k):
+    L = 0
+    longest = 0
+    char_count = {}
+
+    for R in range(len(s)):
+        char = s[R]
+
+        # count char and put in hash map
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+
+        # calculate window
+        window = R - L + 1
+
+        # get most frequency character in hashmap
+        mostF = max(char_count.values())
+
+        # check condition window - mostF = K
+        # if not, keep shrinking the window until condition met
+        while window - mostF > k:
+            char_count[s[L]] -= 1
+            L += 1
+
+            # update window
+            window = R - L + 1
+
+            # update mostF
+            mostF = max(char_count.values())
+
+        longest = max(longest, window)
+
+    return longest
 
 s = "ABABBA"
 k = 2
 
-print(characterReplacement(s, k))
+print(characterReplacement_practice(s, k))
