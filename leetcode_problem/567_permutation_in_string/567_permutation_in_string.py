d7457699f54ceda69de720fa82f0255710b16e53
Thanh Vu <vvt4994@gmail.com>
2024-11-20 22:49:37 -0600

    Update 567_permutation_in_string.py

--- leetcode_problem/567_permutation_in_string/567_permutation_in_string.py
+++ leetcode_problem/567_permutation_in_string/567_permutation_in_string.py
@@ -38,6 +38,59 @@ def checkInclusion(s1, s2):
     return False
 
 
+def checkInclusion_practice(s1, s2):
+    n1, n2 = len(s1), len(s2)
+
+    # edge case
+    if n1 > n2:
+        return False
+
+    s1Count = {}
+    s2Count = {}
+
+    # count char s1
+    for char in s1:
+        if char not in s1Count:
+            s1Count[char] = 1
+        else:
+            s1Count[char] += 1
+
+    L = 0
+
+    for R in range(len(s2)):
+        # only put char that is in s1count
+        char = s2[R]
+        if char in s1Count:
+            if char not in s2Count:
+                s2Count[char] = 1
+            else:
+                s2Count[char] += 1
+
+        window = R - L + 1
+
+        # check when window size > len(s1)
+        # shrinking window until condition met
+        if window > len(s1):
+            # pop the most left element in s2
+            if s2[L] in s2Count:
+                s2Count[s2[L]] -= 1
+            L += 1
+
+        # check if 2 hashcounts equal
+        # yes -> permutation
+        if s1Count == s2Count:
+            return True
+
+    # for loop done -> cant find permutation
+    return False
+
+
+# check permutation
+# count s1
+# count s2
+# compare s1 == s2 --> return True
+# check window, if window > len(s1)
+# shrink window, remove most left character in count hashmap
 s1 = "ab"
-s2 = "eidboaoo"
-print(checkInclusion(s1, s2))
+s2 = "eidbaooo"
+print(checkInclusion_practice(s1, s2))
