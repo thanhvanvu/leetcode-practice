3697efab085c90b6c0b1cca217e19f51a7cb87ae
Thanh Vu <vvt4994@gmail.com>
2024-11-25 16:04:26 -0600

    Update 682_baseball_game.py

--- leetcode_problem/682_baseball_game/682_baseball_game.py
+++ leetcode_problem/682_baseball_game/682_baseball_game.py
@@ -21,10 +21,34 @@ def calPoints(operations):
     return total
 
 
+def calPoints_practice(operations):
+    stack = []
+    for op in operations:
+        if op == "C":
+            stack.pop()
+        elif op == "D":
+            a = stack[-1]
+            result = 2 * int(a)
+            stack.append(result)
+        elif op == "+":
+            a = stack[-1]
+            b = stack[-2]
+            result = int(a) + int(b)
+            stack.append(result)
+        else:
+            stack.append(op)
+
+    score = 0
+    for num in stack:
+        score += int(num)
+
+    return score
+
+
 if __name__ == '__main__':
     ops = ["5", "-2", "4", "C", "D", "9", "+", "+"]
     length_arr = len(ops)
 
-    a = calPoints(ops)
+    a = calPoints_practice(ops)
 
     print('Total', a)
